02d9fc024b88d4c5e75dcba98f586e29786a1d60
juanarancibia <arancibia.juan22@gmail.com>
2023-08-23 10:58:41 -0300

    feat: include date picker in transactions talbe

--- src/components/transaction/TransactionsTable.tsx
+++ src/components/transaction/TransactionsTable.tsx
@@ -9,39 +9,46 @@ import {
   TableRow,
 } from "../ui/table";
 import { MoveRight } from "lucide-react";
+import DatePicker from "../DatePicker";
 
 const TransactionsTable: FC<{}> = () => {
   return (
-    <Table>
-      <TableCaption>A list of the wallet transactions</TableCaption>
+    <>
+      <div className="flex align-center justify-between">
+        <p className="font-semibold text-2xl">Transactions</p>
+        <DatePicker />
+      </div>
+      <Table>
+        <TableCaption>A list of the wallet transactions</TableCaption>
 
-      <TableHeader>
-        <TableRow>
-          <TableHead></TableHead>
-          <TableHead>From</TableHead>
-          <TableHead>To</TableHead>
-          <TableHead>Datetime</TableHead>
-          <TableHead>Value</TableHead>
-          <TableHead>Token</TableHead>
-        </TableRow>
-      </TableHeader>
-
-      {/* Conditions to show red/green depending on receive or withdraw */}
-      <TableBody>
-        {[0, 1, 2, 3, 4, 5, 6].map((_, index) => (
-          <TableRow key={index}>
-            <TableCell className="w-min">
-              <MoveRight className="text-green-500" />
-            </TableCell>
-            <TableCell>0x123...abc</TableCell>
-            <TableCell>0x456...def</TableCell>
-            <TableCell>18/08/2023</TableCell>
-            <TableCell>USDT</TableCell>
-            <TableCell className="text-green-500">+123.456,00</TableCell>
+        <TableHeader>
+          <TableRow>
+            <TableHead></TableHead>
+            <TableHead>From</TableHead>
+            <TableHead>To</TableHead>
+            <TableHead>Datetime</TableHead>
+            <TableHead>Value</TableHead>
+            <TableHead>Token</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+
+        {/* Conditions to show red/green depending on receive or withdraw */}
+        <TableBody>
+          {[0, 1, 2, 3, 4, 5, 6].map((_, index) => (
+            <TableRow key={index}>
+              <TableCell className="w-min">
+                <MoveRight className="text-green-500" />
+              </TableCell>
+              <TableCell>0x123...abc</TableCell>
+              <TableCell>0x456...def</TableCell>
+              <TableCell>18/08/2023</TableCell>
+              <TableCell>USDT</TableCell>
+              <TableCell className="text-green-500">+123.456,00</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 };
 
