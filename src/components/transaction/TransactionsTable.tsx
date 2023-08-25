b592084807780f4656109e94d72485c1f2b6b495
juanarancibia <arancibia.juan22@gmail.com>
2023-08-25 10:55:41 -0300

    feat: change to datatable

--- src/components/transaction/TransactionsTable.tsx
+++ src/components/transaction/TransactionsTable.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import { FC, useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../ui/table";
-import { MoveLeft, MoveRight } from "lucide-react";
-import DatePicker from "../DatePicker";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { WalletSchemaType, selectedWalletState } from "@/states/wallets.atom";
-import { transactionsState } from "@/states/transactions.atom";
 import useAlchemyTransactionsHistory from "@/hooks/useAlchemy";
 import { CustomTransaction, TransactionType } from "@/models/transaction.model";
+import { WalletSchemaType, selectedWalletState } from "@/states/wallets.atom";
+import { ColumnDef } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import DatePicker from "../DatePicker";
+import DataTable from "../ui/DataTable";
+import { MoveLeft, MoveRight } from "lucide-react";
 
-const TransactionsTable: FC<{}> = () => {
+const TransactionsTable = () => {
   const [transactions, setTransactions] = useState<CustomTransaction[]>([]);
   const selectedWallet = useRecoilValue<WalletSchemaType>(selectedWalletState);
   const getAssetTransfers = useAlchemyTransactionsHistory(
@@ -30,6 +22,81 @@ const TransactionsTable: FC<{}> = () => {
       ? selectedWallet.alias
       : `${address.slice(0, 5)}...${address.slice(-5)}`;
   };
+
+  const columns: ColumnDef<CustomTransaction>[] = [
+    {
+      accessorKey: "",
+      header: "",
+      id: "arrow",
+      cell: ({ row }) => {
+        return row.original.transactionType === TransactionType.Incoming ? (
+          <MoveRight className="text-green-600" />
+        ) : (
+          <MoveLeft className="text-red-600" />
+        );
+      },
+    },
+    {
+      accessorKey: "from",
+      header: "From",
+      cell: ({ row }) => formatAddress(row.getValue("from")),
+    },
+    {
+      accessorKey: "to",
+      header: "To",
+      cell: ({ row }) => formatAddress(row.getValue("to")),
+    },
+    {
+      accessorKey: "blockNum",
+      header: "Timestamp",
+    },
+    {
+      accessorKey: "asset",
+      header: "Token",
+    },
+    {
+      accessorKey: "value",
+      header: "Value",
+      cell: ({ row }) => (
+        <p
+          className={`font-semibold ${
+            row.original.transactionType === TransactionType.Incoming
+              ? "text-green-600"
+              : "text-red-600"
+          }`}
+        >
+          {`${
+            row.original.transactionType === TransactionType.Incoming
+              ? "+"
+              : "-"
+          }$${row.getValue("value")}`}
+        </p>
+      ),
+    },
+  ];
+
+  //transactions.map((tx, index) => (
+  //   <TableRow key={index}>
+  //     <TableCell className="w-min">
+  //
+  //     </TableCell>
+  //     <TableCell>{formatAddress(tx.from)}</TableCell>
+  //     <TableCell>{formatAddress(tx.to || "")}</TableCell>
+  //     <TableCell>{tx.blockNum}</TableCell>
+  //     <TableCell>{tx.asset}</TableCell>
+  //     <TableCell
+  //       className={`font-semibold ${
+  //         tx.transactionType === TransactionType.Incoming
+  //           ? "text-green-600"
+  //           : "text-red-600"
+  //       }`}
+  //     >
+  //       {`${
+  //         tx.transactionType === TransactionType.Incoming ? "+" : "-"
+  //       }$${tx.value}`}
+  //     </TableCell>
+  //   </TableRow>
+  // ))}
 
   useEffect(() => {
     Promise.all([
@@ -49,50 +116,11 @@ const TransactionsTable: FC<{}> = () => {
 
         <DatePicker />
       </div>
-      <Table>
-        <TableCaption>A list of the wallet transactions</TableCaption>
-
-        <TableHeader>
-          <TableRow>
-            <TableHead></TableHead>
-            <TableHead>From</TableHead>
-            <TableHead>To</TableHead>
-            <TableHead>Datetime</TableHead>
-            <TableHead>Value</TableHead>
-            <TableHead>Token</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        {/* Conditions to show red/green depending on receive or withdraw */}
-        <TableBody>
-          {transactions.map((tx, index) => (
-            <TableRow key={index}>
-              <TableCell className="w-min">
-                {tx.transactionType === TransactionType.Incoming ? (
-                  <MoveRight className="text-green-600" />
-                ) : (
-                  <MoveLeft className="text-red-600" />
-                )}
-              </TableCell>
-              <TableCell>{formatAddress(tx.from)}</TableCell>
-              <TableCell>{formatAddress(tx.to || "")}</TableCell>
-              <TableCell>{tx.blockNum}</TableCell>
-              <TableCell>{tx.asset}</TableCell>
-              <TableCell
-                className={`font-semibold ${
-                  tx.transactionType === TransactionType.Incoming
-                    ? "text-green-600"
-                    : "text-red-600"
-                }`}
-              >
-                {`${
-                  tx.transactionType === TransactionType.Incoming ? "+" : "-"
-                }$${tx.value}`}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <DataTable
+        columns={columns}
+        data={transactions}
+        tableCaption="A list of the wallet transactions"
+      ></DataTable>
     </>
   );
 };
